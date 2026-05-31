3b490e5f46b0a0fb7547c650f0f15f77ac9c33d4
Swetha <swethaucce@gmail.com>
2026-05-31 08:19:41 +0530

    Add files via upload

---
+++ Learning-Modules/Java/SimpleCalculator.java
@@ -0,0 +1,35 @@
+import java.util.Scanner;
+
+public class SimpleCalculator {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter first number: ");
+        double num1 = sc.nextDouble();
+
+        System.out.print("Enter second number: ");
+        double num2 = sc.nextDouble();
+
+        System.out.print("Enter operation (+, -, *, /): ");
+        char op = sc.next().charAt(0);
+
+        if (op == '+') {
+            System.out.println("Result = " + (num1 + num2));
+        } else if (op == '-') {
+            System.out.println("Result = " + (num1 - num2));
+        } else if (op == '*') {
+            System.out.println("Result = " + (num1 * num2));
+        } else if (op == '/') {
+            if (num2 != 0) {
+                System.out.println("Result = " + (num1 / num2));
+            } else {
+                System.out.println("Cannot divide by zero");
+            }
+        } else {
+            System.out.println("Invalid Operator");
+        }
+
+        sc.close();
+    }
+}

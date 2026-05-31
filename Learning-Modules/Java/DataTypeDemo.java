0a1d60497315d045017d324e138013eba40651fb
Swetha <swethaucce@gmail.com>
2026-05-31 23:55:58 +0530

    Add files via upload

---
+++ Learning-Modules/Java/DataTypeDemo.java
@@ -0,0 +1,17 @@
+public class DataTypeDemo {
+
+    public static void main(String[] args) {
+
+        int number = 10;
+        float decimalNumber = 5.5f;
+        double bigDecimal = 99.99;
+        char letter = 'A';
+        boolean isJavaEasy = true;
+
+        System.out.println("Integer value: " + number);
+        System.out.println("Float value: " + decimalNumber);
+        System.out.println("Double value: " + bigDecimal);
+        System.out.println("Char value: " + letter);
+        System.out.println("Boolean value: " + isJavaEasy);
+    }
+}
